ba120df5a1a08a63b5a254ff2e9d70262257b046
Damian <damianmarti@gmail.com>
2024-07-15 16:08:29 -0300

    Initial commit with 🏗️ Scaffold-ETH 2

---
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useTheme } from "next-themes";
+import { Toaster } from "react-hot-toast";
+import { Client, Provider as URQLProvider, cacheExchange, fetchExchange } from "urql";
+import { WagmiProvider } from "wagmi";
+import { Footer } from "~~/components/Footer";
+import { Header } from "~~/components/Header";
+import { BlockieAvatar } from "~~/components/scaffold-eth";
+import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
+import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
+import { wagmiConfig } from "~~/services/web3/wagmiConfig";
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  useInitializeNativeCurrencyPrice();
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="relative flex flex-col flex-1">{children}</main>
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const urqlClient = new Client({
+  url: process.env.NEXT_PUBLIC_PONDER_URL || "http://localhost:42069",
+  exchanges: [cacheExchange, fetchExchange],
+});
+
+export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
+  const { resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ProgressBar />
+        <RainbowKitProvider
+          avatar={BlockieAvatar}
+          theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
+        >
+          <URQLProvider value={urqlClient}>
+            <ScaffoldEthApp>{children}</ScaffoldEthApp>
+          </URQLProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
---
+++ packages/ponder/README.md
@@ -0,0 +1,7 @@
+# SE-2 Ponder extension
+
+This extension allow to use Ponder (https://ponder.sh/) for event indexing on a SE-2 dapp.
+
+## Sample queries
+
+TODO: complete doc
